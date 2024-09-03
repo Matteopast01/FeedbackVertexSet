90094b49857876aa66446d0b563d9c509eeaf392
Matteopast01 <matteopast@gmail.com>
2024-09-03 18:42:12 +0200

    Adding Acyclic Graph Generator

--- graph_generators/AcyclicGraphGenerator.py
+++ graph_generators/AcyclicGraphGenerator.py
@@ -19,9 +19,8 @@ class AcyclicGraphGenerator:
         bfs_queue = [start_node]
         visited[start_node] = True
         nodes_in_tree = 1
-        node_mapping = {start_node: 0}  # Mapping of original node to new node index
-        node_index = 1  # Start indexing for new nodes
-
+        node_mapping = {start_node: 0} 
+        node_index = 1 
         while bfs_queue and nodes_in_tree < self._number_nodes:
             u = bfs_queue.pop(0)
             for v in dense_graph.iterNeighbors(u):
@@ -35,10 +34,9 @@ class AcyclicGraphGenerator:
                     if nodes_in_tree >= self._number_nodes:
                         break
         
-        # Create a new graph for the spanning tree
         spanning_tree = nk.graph.Graph(self._number_nodes, weighted=False, directed=False)
         
-        # Add edges to the spanning tree with the new node indices
+
         for u, v in spanning_tree_edges:
             if u in node_mapping and v in node_mapping:
                 new_u = node_mapping[u]
